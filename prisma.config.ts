835e3540829514d8932f85f80ee84babc3547cc4
ggdevlaptop <gonzalo.a.gomez22@gmail.com>
2026-04-05 21:07:21 -0300

    Update prisma.config.ts.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,21 +1,12 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-import path from "path";
-import { fileURLToPath } from "url";
-import { config } from "dotenv";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const envPath = path.join(__dirname, "..", ".env");
-config({ path: envPath });
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "API/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "API/prisma/migrations",
   },
   datasource: {
-    url: env("DIRECT_URL"),
+    url: process.env.DIRECT_URL,
   },
 });
