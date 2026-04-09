4caf0b7159d6a059777edd32cd533fa8887d3456
ggdevlaptop <gonzalo.a.gomez22@gmail.com>
2026-04-09 06:05:20 -0300

    Remove CORS.

--- API/app.js
+++ API/app.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { config } from "dotenv";
 import express from "express";
 import router from "./router/router.js";
-import cors from "cors";
+// import cors from "cors";
 import { envPath } from "./dotenvconfig.js";
 
 const app = express();
@@ -11,12 +11,12 @@ config({ path: envPath });
 
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   }),
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
